b0889c0cb78f19f172d12e8bfe65f4dba0194c3f
HMDLong <long.hmd2000@gmail.com>
2021-12-14 16:46:09 +0000

    add home

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+    
+    validates :name, presence: true
+    validates :phone, presence: true
+    validates :address, presence: true
 end
