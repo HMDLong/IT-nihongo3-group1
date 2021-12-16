d7dbaf866add35326a16fe51cc5715009e3f0fc9
HMDLong <long.hmd2000@gmail.com>
2021-12-16 07:24:10 +0000

    fix sign_out bug

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,8 @@ Rails.application.routes.draw do
   resources :foods
   root 'static_pages#home'
   devise_for :users
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
